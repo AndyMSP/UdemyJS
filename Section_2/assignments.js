e90a97ac5beb0ef7b7b56cc827c17e7f8e1cb62e
Andy <astonemn@gmail.com>
2024-02-13 11:34:26 -0400

    add lesson 9 assignment to assignments.js

---
+++ Section_2/assignments.js
@@ -0,0 +1,13 @@
+let country = "USA";
+let continent = "North America";
+let population = 350000000;
+
+console.log(
+  "The country " +
+    country +
+    " is located in " +
+    continent +
+    " and has " +
+    population +
+    " people"
+);
